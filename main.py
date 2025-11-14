54785b89d5795ecdf0e23c909147503c2f16f64a
Xseis <114229348+Casper6ybaka@users.noreply.github.com>
2025-11-14 11:01:48 +0100

    update

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+
+totalmessages = 0
+
+for file in os.listdir("package"):
+    if file == "Account":
+        pass
+    elif file == "Activities":
+        pass
+    elif file == "Activity":
+        pass
+    elif file == "Ads":
+        pass
+    elif file == "Messages":
+        pass
+    print(file)
